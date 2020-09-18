fd134ff2411be71a067a887bc343ebfa2ce7ab07
Michael Clifford <cliffom@gmail.com>
2020-09-18 14:22:16 -0700

    Add methods to get a list of projects

--- wistia.go
+++ wistia.go
@@ -1,6 +1,9 @@
 package wistia
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -20,6 +23,46 @@ type Client struct {
 
 	// HTTPClient is the client that makes the HTTP requests
 	HTTPClient *http.Client
+}
+
+// ProjectsList returns a list of projects from Wistia
+func (c *Client) ProjectsList(ctx context.Context) (*Projects, error) {
+	endpoint := fmt.Sprintf("%s/projects.json?access_token=%s", c.BaseURL, c.accessToken)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req = req.WithContext(ctx)
+
+	res := Projects{}
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+func (c *Client) sendRequest(req *http.Request, v interface{}) error {
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
+		return fmt.Errorf("unknown error, status code: %d", res.StatusCode)
+	}
+
+	if err = json.NewDecoder(res.Body).Decode(&v); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // NewClient returns a pointer to Client
