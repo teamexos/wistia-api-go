84d5a1c71c4232d931ec2e3ffce4d6e88184fd8a
Michael Clifford <cliffom@gmail.com>
2020-09-18 19:32:38 -0700

    Added TestProjectsList

--- wistia_test.go
+++ wistia_test.go
@@ -14,13 +14,15 @@ import (
 )
 
 var (
-	restClient *mocks.MockHTTPClient
-	ctx        context.Context
+	restClient   *mocks.MockHTTPClient
+	ctx          context.Context
+	wistiaClient *wistia.Client
 )
 
 func init() {
 	restClient = &mocks.MockHTTPClient{}
 	ctx = context.Background()
+	wistiaClient = wistia.NewClient(restClient, "access_token")
 }
 
 func TestMediasShow(t *testing.T) {
@@ -32,8 +34,7 @@ func TestMediasShow(t *testing.T) {
 		}, nil
 	}
 
-	c := wistia.NewClient(restClient, "access_token")
-	medias, err := c.MediasShow(ctx, "fakeID", nil)
+	medias, err := wistiaClient.MediasShow(ctx, "fakeID", nil)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, medias)
@@ -42,4 +43,28 @@ func TestMediasShow(t *testing.T) {
 	assert.EqualValues(t, "How They Work", medias.Project.Name)
 	assert.EqualValues(t, "ln2k6qwi9k", medias.Project.HashedID)
 	assert.EqualValues(t, 4, len(medias.Assets))
+}
+
+func TestProjectsList(t *testing.T) {
+
+	mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       fixtures.ProjectsList(),
+		}, nil
+	}
+
+	projects, err := wistiaClient.ProjectsList(ctx, nil)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, projects)
+	assert.EqualValues(t, 2, len(*projects))
+
+	for _, project := range *projects {
+		assert.NotNil(t, project.ID)
+		assert.NotNil(t, project.Name)
+		assert.NotNil(t, project.MediaCount)
+		assert.NotNil(t, project.HashedID)
+		assert.NotNil(t, project.PublicID)
+	}
 }
